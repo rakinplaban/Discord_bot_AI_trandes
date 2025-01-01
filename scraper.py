54ab4fae2b3dedf10d0d7b00df4b757f94f8964b
rakinplaban <rakinshahriar54@gmail.com>
2025-01-01 21:07:55 +0600

    Image update

---
+++ scraper.py
@@ -0,0 +1,28 @@
+import requests
+import json
+import bs4 as bs
+
+headers = {
+    # 'authority': 'www.ainews.com',
+    'referer': 'https://www.ainews.com/',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-language': 'en-US,en;q=0.9',
+    'cache-control': 'no-cache',
+    'cookie': 'theme=dark',
+    'pragma': 'no-cache',
+    'sec-ch-ua': '"Not?A_Brand";v="8", "Chromium";v="108", "Google Chrome";v="108"',
+    'sec-ch-ua-mobile': '?0',
+    'sec-ch-ua-platform': '"Linux"',
+    'sec-fetch-dest': 'document',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-site': 'cross-site',
+    'sec-fetch-user': '?1',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',
+}
+
+url = 'https://www.ainews.com/p/5-ai-scams-poised-to-surge-in-2025-and-how-to-stay-safe'
+
+response = requests.get(url, headers=headers)
+
+print(response.status_code)
